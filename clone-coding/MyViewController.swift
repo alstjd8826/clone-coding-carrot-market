aa1f938b82d3db676ca895c2da0f5d1f0e1881e2
minsungKim <htk4064@gmail.com>
2021-07-08 23:38:55 +0900

    스위프트 당근마켓 클론코딩

---
+++ clone-coding/MyViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  clone-coding
+//
+//  Created by 김민성 on 2021/07/08.
+//
+
+import UIKit
+
+class MyViewController: UIViewController {
+
+    convenience init(title:String, bgColor: UIColor) {
+        self.init()
+        
+        self.title = title
+        self.view.backgroundColor = bgColor
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
--- clone-coding/SceneDelegate.swift
+++ clone-coding/SceneDelegate.swift
@@ -16,7 +16,25 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        
+        // 윈도우 씬 가져오기
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        // 윈도우 크기 결정
+        window = UIWindow(frame: UIScreen.main.bounds)
+        
+        // 뷰컨트롤러 인스턴스 설정
+        let mainTC = MainTabBarController()
+        
+        // 루트 뷰 컨트롤러를 메인탭바로 설정
+        window?.rootViewController = mainTC
+        
+        // 설정한 윈도우를 보이게 끔 설정
+        window?.makeKeyAndVisible()
+        
+        // 윈도우 씬 설정
+        window?.windowScene = windowScene
+        
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
